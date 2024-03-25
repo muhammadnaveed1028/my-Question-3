2a1c3b7a720ca57c174ebbfd5a79149dd2ad6320
muhammadnaveed1028 <163102918+muhammadnaveed1028@users.noreply.github.com>
2024-03-25 12:13:32 -0700

    Add files via upload

---
+++ qaid.js
@@ -0,0 +1,8 @@
+var nameCase = "Qaid-E-Azam";
+console.log(nameCase);
+var lowerCase = "muhammad ali jinah";
+console.log(lowerCase);
+var upercase = "Muhammad Ali Jinah";
+console.log(upercase);
+var titleCase = " Founder of Pakistan";
+console.log(titleCase);
---
+++ qaid.ts
@@ -0,0 +1,8 @@
+let nameCase = "Qaid-E-Azam";
+console.log(nameCase);
+let lowerCase = "muhammad ali jinah";
+console.log(lowerCase);
+let upercase = "Muhammad Ali Jinah";
+console.log(upercase);
+let titleCase = " Founder of Pakistan";
+console.log(titleCase);
